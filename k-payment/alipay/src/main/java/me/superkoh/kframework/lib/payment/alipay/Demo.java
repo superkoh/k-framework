2e16674d93ce238ea9985664a1d7145e3eb18abc
王乐天 <koh.718@gmail.com>
2017-05-21 23:22:37 +0800

    lib.payment: init

---
+++ k-payment/alipay/src/main/java/me/superkoh/kframework/lib/payment/alipay/Demo.java
@@ -0,0 +1,9 @@
+package me.superkoh.kframework.lib.payment.alipay;
+
+/**
+ * Created by KOH on 2017/5/21.
+ * <p>
+ * k-framework
+ */
+public class Demo {
+}
---
+++ k-payment/union/src/main/java/me/superkoh/kframework/lib/payment/union/Demo.java
@@ -0,0 +1,9 @@
+package me.superkoh.kframework.lib.payment.union;
+
+/**
+ * Created by KOH on 2017/5/21.
+ * <p>
+ * k-framework
+ */
+public class Demo {
+}
