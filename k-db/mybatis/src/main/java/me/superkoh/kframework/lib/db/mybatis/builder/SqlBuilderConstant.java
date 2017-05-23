013fdb47769c0609b24a9a599f51b177541a806c
王乐天 <koh.718@gmail.com>
2017-05-23 13:42:43 +0800

    lib.db.mybatis: 增加sqlbuilder支持操作的常量

---
+++ k-db/mybatis/src/main/java/me/superkoh/kframework/lib/db/mybatis/builder/SqlBuilderConstant.java
@@ -0,0 +1,14 @@
+package me.superkoh.kframework.lib.db.mybatis.builder;
+
+/**
+ * Created by KOH on 2017/5/23.
+ * <p>
+ * k-framework
+ */
+public class SqlBuilderConstant {
+    public static final String INSERT = "insert";
+    public static final String UPDATE_BY_PRIMARY_KEY_SELECTIVE = "updateByPrimaryKeySelective";
+    public static final String SELECT_BY_QUERY = "selectByQuery";
+    public static final String SELECT_PAGE_BY_QUERY = "selectPageByQuery";
+    public static final String COUNT_BY_QUERY = "countByQuery";
+}
